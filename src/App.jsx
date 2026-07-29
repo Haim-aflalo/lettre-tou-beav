2d9713fb220609c528420d7a83df4c219757017e
Haim Daniela Flalo <kvim.hda@gmail.com>
2026-07-29 12:34:29 +0300

    Ajouter la lettre React et la musique

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <main className="site-shell">
+      <iframe
+        className="experience"
+        src="./experience.html"
+        title="Pour toi, mon amour"
+        allow="autoplay"
+      />
+    </main>
+  );
+}
